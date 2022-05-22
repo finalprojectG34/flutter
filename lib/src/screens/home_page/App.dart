1b9e4b0cb814a13360ed22bf009cc79fec8d1ae1
motidinsa <dinsamoti@gmail.com>
2022-05-22 08:34:50 +0300

    search page done

--- lib/src/screens/home_page/App.dart
+++ lib/src/screens/home_page/App.dart
@@ -9,6 +9,7 @@ import 'package:sms/src/screens/screens.dart';
 
 import '../../../package_delivery_tracking.dart';
 import '../auth/login/login.dart';
+import '../cart/cart.dart';
 import '../components/single_item_search_component.dart';
 import 'AppCtx.dart';
 import 'item_detail/item_detail.dart';
@@ -90,8 +91,8 @@ class _AppState extends State<App> {
       // CategoriesPage();
       case 2:
         appController.changePage('Cart', index);
-        return PackageDeliveryTrackingPage();
-      // return Cart();
+        // return PackageDeliveryTrackingPage();
+        return Cart();
       // if (state.status == AuthenticationStatus.authenticated) {
       //   return ProfilePage();
       // }
---
+++ lib/src/screens/search_item/search_item_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_getx_widget.dart';
+import 'package:sms/src/screens/search_item/search_item_ctx.dart';
+
+import '../components/single_item_search_component.dart';
+
+class SearchItemList extends StatelessWidget {
+  final String searchItemName;
+
+  const SearchItemList({Key? key, required this.searchItemName})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(searchItemName)),
+      body: GetX<SearchController>(builder: (ctx) {
+        return ctx.isLoading.isTrue
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemBuilder: (context, index) => SingleItemSearch(
+                    variable: ctx.mockSearchItem!['getAllItems'][index]),
+                itemCount: ctx.mockSearchItem!['getAllItems'].length,
+              );
+      }),
+    );
+  }
+}
