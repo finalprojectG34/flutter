fc041f803cab5672e0be68eba82501f807e5f3c7
Yonatan Endale <yonathanendu@gmail.com>
2022-05-28 15:06:40 +0300

    cart ui modification

--- lib/helper/constance.dart
+++ lib/helper/constance.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 const kTileHeight = 50.0;
 const inProgressColor = Colors.black;
 const todoColor = Color(0xffd1d2d7);
+const color1=Color(0xff223263);
+const color2=Color(0xffEBF0FF);
 
 enum Pages {
   DeliveryTime,
--- lib/src/screens/components/payment_detail.dart
+++ lib/src/screens/components/payment_detail.dart
@@ -1,71 +1,70 @@
 import 'package:flutter/material.dart';
 
+import '../../../helper/constance.dart';
+
 class PaymentDetail extends StatelessWidget {
   const PaymentDetail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    'Items (2)',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  Text(
-                    'ETB 46.22',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    'Shipping',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  Text(
-                    'ETB 50.22',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Divider(
-                height: 10,
-                color: Colors.grey,
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    'Total Price',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                  Text(
-                    'ETB 150.22',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ],
-              ),
-            ],
-          ),
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Items (2)',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                Text(
+                  'ETB 46.22',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Shipping',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                Text(
+                  'ETB 50.22',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Divider(
+              height: 10,
+              color: Colors.grey,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Total Price',
+                  style: TextStyle(color: color1, fontSize: 16,fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'ETB 150.22',
+                  style: TextStyle(fontSize: 16,color: Colors.lightBlue),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
